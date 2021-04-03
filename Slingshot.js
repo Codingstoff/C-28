403624a5eed203e6cc6254e0e134f9c631d82ee2
Codingstoff <73205292+Codingstoff@users.noreply.github.com>
2021-04-03 11:48:38 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,36 @@
+class Slingshot 
+{
+   constructor (body1,point2)
+   {
+    var options =
+    {
+       bodyA: body1,
+       pointB: point2,
+       stiffness: 0.1,
+       length: 1
+
+    }
+
+    this.slingshot = Constraint.create (options);
+    World.add(world,this.slingshot);
+   }
+
+    display()
+    {
+      if (this.slingshot.bodyA)
+      {
+         var body1pos = this.slingshot.bodyA.position;
+         var body2pos = this.slingshot.pointB;
+       
+      line(body1pos.x, body1pos.y,body2pos.x,body2pos.y); 
+      }
+
+    }
+
+      
+
+    fly()
+    {
+      this.slingshot.bodyA= null;
+    }
+}
